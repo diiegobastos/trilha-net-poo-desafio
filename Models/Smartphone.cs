cfd8108dd79507b2db7dcbe91384022f7bdb439b
Diego Bastos <dicabralpersonal@gmail.com>
2025-07-29 10:59:12 -0300

    to do's realizados

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,25 @@
+using System.Runtime.CompilerServices;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public string Numero { get; set; }
+        private string _modelo;
+        private string _imei;
+        private int _memoria;
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+
         {
-            Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Numero = numero;
+            _modelo = modelo;
+            _imei = imei;
+            _memoria = memoria;
+
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        // TODO: Realizar os testes com as classes Nokia e Iphone
+
+        Console.WriteLine("Smartphone Nokia");
+        Smartphone nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Whatsapp");
+
+        Console.WriteLine("\nSmartphone Iphone");
+        Smartphone iphone = new Iphone("987654321", "Iphone 16", "98765432109876", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
